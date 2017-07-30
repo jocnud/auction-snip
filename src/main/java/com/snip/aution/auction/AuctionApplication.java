6928250b18c1964e56b72108ae298ec15a60b5f1
Shahbaz <jocnud@gmail.com>
2017-07-30 21:10:55 +0530

    Initial Checkin

---
+++ src/main/java/com/snip/aution/auction/AuctionApplication.java
@@ -0,0 +1,24 @@
+package com.snip.aution.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionApplication.class, args);
+	}
+}
+
+
+@RestController
+class Test{
+	
+	@GetMapping
+	public String say(){
+		return "hello";
+	}
+}
